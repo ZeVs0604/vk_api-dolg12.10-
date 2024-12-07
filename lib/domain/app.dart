a26bd27b5b24f492878d01b2b85430ae06e2b130
ZeVs <s521411060@ya.ru>
2024-12-07 17:57:57 +0300

    Отработка за 12.10 (разделение на Dto и Domain)

---
+++ lib/domain/app.dart
@@ -0,0 +1,14 @@
+class App {
+  final String type;
+  final int id;
+  final String title;
+  final int mobileControlsType;
+
+  App({
+    required this.type,
+    required this.id,
+    required this.title,
+    required this.mobileControlsType,
+  });
+
+}
